96fb17c4108df312ee3adaacd3d23d1c3e30b59d
DeniseCastroUOL <82236516+DeniseCastroUOL@users.noreply.github.com>
2021-05-25 10:14:51 -0300

    Atualizando views

--- app/src/main/java/br/com/trilhamobile/appnews/activity/FavoriteActivity.kt
+++ app/src/main/java/br/com/trilhamobile/appnews/activity/FavoriteActivity.kt
@@ -1,11 +1,12 @@
-package br.com.trilhamobile.appnews
+package br.com.trilhamobile.appnews.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.trilhamobile.appnews.R
 
-class MainActivity : AppCompatActivity() {
+class FavoriteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_favorite)
     }
 }
