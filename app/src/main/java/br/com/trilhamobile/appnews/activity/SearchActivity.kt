6176d7ab0e50a388d513818faed4a0e36542bc6f
DeniseCastroUOL <82236516+DeniseCastroUOL@users.noreply.github.com>
2021-05-25 13:38:00 -0300

    activity_search

---
+++ app/src/main/java/br/com/trilhamobile/appnews/activity/SearchActivity.kt
@@ -0,0 +1,12 @@
+package br.com.trilhamobile.appnews.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.trilhamobile.appnews.R
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
