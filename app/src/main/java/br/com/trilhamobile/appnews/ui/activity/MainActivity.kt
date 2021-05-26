41a3772fefaed5624d7ed8da817202b6f1a9811a
DeniseCastroUOL <82236516+DeniseCastroUOL@users.noreply.github.com>
2021-05-26 11:35:00 -0300

    Listando notícias

--- app/src/main/java/br/com/trilhamobile/appnews/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/trilhamobile/appnews/ui/activity/MainActivity.kt
@@ -2,12 +2,60 @@ package br.com.trilhamobile.appnews.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.trilhamobile.appnews.R
+import br.com.trilhamobile.appnews.adapter.MainAdapter
+import br.com.trilhamobile.appnews.model.Article
+import br.com.trilhamobile.appnews.model.data.NewsDataSource
+import br.com.trilhamobile.appnews.presenter.ViewHome
+import br.com.trilhamobile.appnews.presenter.news.NewsPresenter
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AbstractActivity() {
+class MainActivity : AbstractActivity(), ViewHome.View {
+
+    private val mainAdapter by lazy {
+        MainAdapter()
+    }
+
+    private lateinit var presenter: NewsPresenter
+
     override fun getLayout(): Int = R.layout.activity_main
 
     override fun onInject() {
-        TODO("Not yet implemented")
+
+        val dataSource = NewsDataSource()
+        presenter = NewsPresenter(this, dataSource)
+        presenter.requestAll()
+        configRecycle()
+    }
+
+    private fun configRecycle() {
+        with(rvNews) {
+            adapter = mainAdapter
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            addItemDecoration(DividerItemDecoration(
+                    this@MainActivity,
+                    DividerItemDecoration.VERTICAL
+            ))
+        }
+    }
+
+    override fun showProgressBar() {
+        rvProgressBar.visibility = View.VISIBLE
+    }
+
+    override fun showFailure(message: String) {
+        Toast.makeText(this, "", Toast.LENGTH_LONG).show()
+    }
+
+    override fun hideProgressBar() {
+        rvProgressBar.visibility = View.INVISIBLE
+    }
+
+    override fun showArticles(articles: List<Article>) {
+        mainAdapter.differ.submitList(articles.toList())
     }
 }
