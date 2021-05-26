009547923f3551583e78df6731409e4a7ab767c3
DeniseCastroUOL <82236516+DeniseCastroUOL@users.noreply.github.com>
2021-05-26 13:41:53 -0300

    NewsDataSource - SearchNews

--- app/src/main/java/br/com/trilhamobile/appnews/model/data/NewsDataSource.kt
+++ app/src/main/java/br/com/trilhamobile/appnews/model/data/NewsDataSource.kt
@@ -3,6 +3,7 @@ package br.com.trilhamobile.appnews.model.data
 import android.media.browse.MediaBrowser
 import br.com.trilhamobile.appnews.network.RetrofitInstance
 import br.com.trilhamobile.appnews.presenter.news.NewsHome
+import br.com.trilhamobile.appnews.presenter.search.SearchHome
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -14,6 +15,21 @@ class NewsDataSource {
             val response = RetrofitInstance.api.getBreakingNews("br")
             if (response.isSuccessful) {
                 response.body()?.let {newsResponse ->
+                    callback.onSuccess(newsResponse)
+                }
+                callback.onComplete()
+            } else {
+                callback.onError(response.message())
+                callback.onComplete()
+            }
+        }
+    }
+
+    fun searchNews(term: String, callback: SearchHome.Presenter) {
+        GlobalScope.launch(Dispatchers.Main) {
+            val response = RetrofitInstance.api.searchNews(term)
+            if (response.isSuccessful) {
+                response.body()?.let{newsResponse ->
                     callback.onSuccess(newsResponse)
                 }
                 callback.onComplete()
